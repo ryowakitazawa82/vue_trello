d14a5117172ff898299686553a00b23cffc8b8cc
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-11-29 17:39:05 +0900

    finished 3-4

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,47 @@
+<template>
+  <form :class="classList" @submit.prevent="addList">
+    <input v-model="title"
+            type="text"
+            class="text-input"
+            placeholder="Add new list"
+            @focusin="startEditing"
+            @focusout="finishEditing"
+    >
+    <button type="submit"
+            class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: '',
+      isEditing: false,
+    }
+  },
+  computed: {
+    classList() {
+      const classList = ['addlist']
+      if (this.isEditing) {
+        classList.push('active')
+      }
+      return classList
+    },
+  },
+  methods: {
+    addList: function() {
+      this.$store.dispatch('addlist', { title: this.title })
+      this.title = ''
+    },
+    startEditing: function() {
+      this.isEditing = true
+    },
+    finishEditing: function() {
+    this.isEditing = false
+    },
+  }
+}
+</script>
